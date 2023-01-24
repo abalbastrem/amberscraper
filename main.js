2691f911a554823b2f3ab8b603b0347c39741518
abalbastrem <albert.balbastre@gmail.com>
2023-01-24 01:10:00 +0100

    nightly

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function serverStart() {
     const playwright = require('playwright');
     (async () => {
         const browser = await playwright.firefox.launch({
-            headless: false,
+            headless: true,
             // args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const context = await browser.newContext();
@@ -34,14 +34,43 @@ function serverStart() {
 
         const highlightedTexts = await page.locator('span[style="background-color: rgba(0, 90, 80, 0.35);"] > span').allInnerTexts();
         var hlBlobArr = [];
+        // pseudo element ::first-letter, ::before, ::after, with lambdas?
         for (hlText of highlightedTexts) {
             hlBlob = new Map();
-            hlSpan = await page.getByText(hlText);
-            // click in the first word of hlSpan
-            words = firstNLastWords(hlText);
             console.log(hlText);
-            console.log(words);
-            await hlSpan.click({modifiers:['Alt']});
+            let hlSpan = await page.getByText(hlText);
+
+            // TODO get hlSpan inside the evaluate with JS
+            const hlSpanMod = await page.evaluateHandle(
+                e1 => {
+                    const first = document.createElement('div');
+                    const last = document.createElement('div');
+                    first.setAttribute('id', 'first');
+                    last.setAttribute('id', 'last');
+                    e1.append(first);
+                    e1.prepend(last);
+                    return e1;
+                }, hlSpan
+            );
+            // const hlSpanMod = await page.evaluate(
+            //     three => {return 2 + three}, 3
+            //     );
+            console.log(hlSpan);
+            console.log(hlSpanMod);
+            // locate element by id
+            const firstEl = await page.locator("#first").first();
+            const lastEl = await page.locator("#last").first();
+
+            console.log(firstEl);
+            console.log(lastEl);
+
+            await firstEl.click({modifiers:['Alt']});
+            await lastEl.click({modifiers:['Alt']});
+
+            return;
+            words = firstNLastWords(hlText);
+            return;
+            // await hlSpan.click({modifiers:['Alt']});
             const tcIn = await getTimecode(page);
             hlBlob.set("tc_in", tcIn);
             // hlBlob.set("tc_out", tcOut);
@@ -51,7 +80,13 @@ function serverStart() {
     })();
 }
 
-async function firstNLastWords(text) {
+async function lambda() {
+    return await element.evaluate((e1) => {
+        return window.getComputedStyle(e1,':after').top
+     })
+}
+
+function firstNLastWords(text) {
     const textByWords = text.split(' ');
     const firstWord = textByWords[0];
     const lastWord = textByWords[textByWords.length-1];
@@ -63,7 +98,7 @@ async function firstNLastWords(text) {
 }
 
 async function clickOnWord(element, word) {
-    await element.evaluate((element, word) => {
+    return await element.evaluate((element, word) => {
         const selection = window.getSelection();
         const range = document.createRange();
         range.setStart(element.childNodes[0], content.indexOf(word));
@@ -75,7 +110,7 @@ async function clickOnWord(element, word) {
 
 async function altClickOutTimecode(highlightedTextElement) {
     await highlightedTextElement.focus();
-    await highlightedTextElement.setSelectionRange(0, 0, "backward");
+    await highlightedTextElement.setSelectionRange(0, 0, "backward"); // TODO use this in a lambda?
     await page.keyboard.down('Alt');
     await highlightedTextElement.click();
     await page.keyboard.up('Alt');
