a777b757b3e176fc596fc302661b3aed29ff932e
abalbastrem <albert.balbastre@gmail.com>
2023-01-31 00:48:30 +0100

    nightly

--- main.js
+++ main.js
@@ -9,73 +9,74 @@ function serverStart() {
     const playwright = require('playwright');
     (async () => {
         const browser = await playwright.firefox.launch({
-            headless: true,
+            headless: false,
             // args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
+        console.log("start");
         const context = await browser.newContext();
         const page = await context.newPage();
 
+        console.log("login page");
         await page.goto(config.URL_LOGIN);
         await page.type('input[placeholder="Email"]', config.USER_EMAIL);
         await page.type('input[placeholder="Password"]', config.USER_PASSWORD);
         await page.click('button[data-testid="login-btn-login"]');
         await page.waitForNavigation();
 
+        console.log("video page");
         await page.goto(config.URL_VIDEO, { waitUntil: 'domcontentloaded', timeout: 0 });
         await page.waitForNavigation();
         page.waitForSelector('div[class="WavePlayer__highlight"]');
         await page.waitForSelector('button[data-type="close"]');
         await page.click('button[data-type="close"]');
 
+        console.log("hide annoying elements");
         await hideElement(page, 'footer[class="footer_main"]');
         await hideElement(page, 'div[class="WavePlayer__mediawrap isVideo isExpanded"]');
         await hideElement(page, 'header[class="AppBar"]');
         
-
+        console.log("locate highlighted texts");
         const highlightedTexts = await page.locator('span[style="background-color: rgba(0, 90, 80, 0.35);"] > span').allInnerTexts();
         var hlBlobArr = [];
-        // pseudo element ::first-letter, ::before, ::after, with lambdas?
+        let counter = 0;
         for (hlText of highlightedTexts) {
             hlBlob = new Map();
-            console.log(hlText);
-            let hlSpan = await page.getByText(hlText);
+            let hlSpan = await page.getByText(hlText); // DEBUG too generic texts sometimes!
 
-            // TODO get hlSpan inside the evaluate with JS
-            const hlSpanMod = await page.evaluateHandle(
-                e1 => {
-                    const first = document.createElement('div');
-                    const last = document.createElement('div');
-                    first.setAttribute('id', 'first');
-                    last.setAttribute('id', 'last');
-                    e1.append(first);
-                    e1.prepend(last);
-                    return e1;
-                }, hlSpan
-            );
-            // const hlSpanMod = await page.evaluate(
-            //     three => {return 2 + three}, 3
-            //     );
-            console.log(hlSpan);
-            console.log(hlSpanMod);
-            // locate element by id
-            const firstEl = await page.locator("#first").first();
-            const lastEl = await page.locator("#last").first();
+            const firstId = "first_"+counter;
+            const lastId = "last_"+counter;
 
-            console.log(firstEl);
-            console.log(lastEl);
+            await hlSpan.evaluate((el, num) => {
+                const first = document.createElement('span');
+                first.setAttribute('id', "first_"+num);
+                first.setAttribute('style', "background-color: rgba(0, 90, 80, 0.35)");
+                first.setAttribute('data-text', 'true');
+                first.innerText ="CH"+num;
+                el.insertAdjacentElement('beforebegin', first);
+            }, counter);
 
-            await firstEl.click({modifiers:['Alt']});
-            await lastEl.click({modifiers:['Alt']});
-
-            return;
-            words = firstNLastWords(hlText);
-            return;
-            // await hlSpan.click({modifiers:['Alt']});
+            await page.click("#"+firstId, {modifiers:['Alt'], timeout: 5000});
             const tcIn = await getTimecode(page);
+
+            await hlSpan.evaluate((el, num) => {
+                const last = document.createElement('span');
+                last.setAttribute('id', "last_"+num);
+                // last.setAttribute('style', "background-color: rgba(0, 90, 80, 0.35)");
+                last.setAttribute('style', "float: right;");
+                last.setAttribute('data-text', 'true');
+                last.innerText ="CH"+num;
+                el.insertAdjacentElement('beforeend', last);
+            }, counter);
+
+            await page.click("#"+lastId, {modifiers:['Alt'], timeout: 5000});
+            const tcOut = await getTimecode(page);
+
             hlBlob.set("tc_in", tcIn);
-            // hlBlob.set("tc_out", tcOut);
+            hlBlob.set("tc_out", tcOut);
             hlBlob.set("text", hlText);
-            // console.log(hlBlob);
+            console.log(hlBlob);
+
+            counter++;
         }
     })();
 }
